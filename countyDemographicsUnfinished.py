426422fcdbc74c3b9704daa7fd5bffc4ab2ba3a9
P-Nasty <31251496+P-Nasty@users.noreply.github.com>
2017-11-06 09:34:08 -0800

    Update countyDemographicsUnfinished.py

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -1,122 +1,55 @@
-from flask import flask, url_for,render_template, request, Markup, flash
-import json
-import os
-app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+from flask import Flask, url_for, render_template, request, Markup, flash
+import os, json, random
+
+
+app = Flask(__name__)
+
+with open('county_demographics.json') as demographics_data:
+       counties = json.load(demographics_data)
+
 @app.route("/")
 def render_main():
-   return render_template('demographics.html')
-   """ with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-        state = get_state_options
-    get_state_options(counties)
-    fun_fact(state)"""
-    
+    return render_template('home.html', options = get_state_options())
 
-"""def alphabetically_first_county(counties):
-    Return the county with the name that comes first alphabetically.
-    first = counties[0]["County"]
-    for c in counties:
-        if c["County"]< first:
-            first = c["County"]
-    return first
+@app.route("/home")
+def render_response():
+    state = request.args["state"]
+    return render_template('home.html', options = get_state_options(),response = your_interesting_demographic_function(state))
 
-
-def county_most_under_18(counties):
-    first = counties[0]["Age"]["Percent Under 18 Years"]
-    second = counties[0]["County"]
-    third = counties[0]["State"]
-    for c in counties:
-        if c["Age"]["Percent Under 18 Years"] > first:
-            first = c["Age"]["Percent Under 18 Years"] 
-            second = c["County"]
-            third = c["State"]
-    return second, third
-    
-    
-
-    
-def percent_most_under_18(counties):
-    first = counties[0]["Age"]["Percent Under 18 Years"]
-    for c in counties:
-        if c["Age"]["Percent Under 18 Years"] > first:
-            first = c["Age"]["Percent Under 18 Years"]
-    return first
-  
-
-    
-def most_under_18(counties):
-    first = counties[0]["Age"]["Percent Under 18 Years"]
-    second = counties[0]["County"]
-    third = counties[0]["State"]
-    for c in counties:
-        if c["Age"]["Percent Under 18 Years"] > first:
-            first = c["Age"]["Percent Under 18 Years"] 
-            second = c["County"]
-            third = c["State"]
-    fourth = [first,second,third]
-    return fourth
-    
-
-    
-def state_with_most_counties(counties):
-    dict = {"AL":0}
-    first = counties[0]["State"]
-    second = 0
-    third = counties[0]["State"]
-    for c in counties: 
-        first = c["State"]
-        if first in dict:
-            dict[first] += 1 
-        else:
-            dict[first] = 1
-    for c in dict:
-        if dict[c] > second:
-            second = dict[c]
-            third = c
-    return third
-            
+def get_state_options():
         
-  
-    #Make a dictionary that has a key for each state and the values keep track of the number of counties in each state
-    
-    #Find the state in the dictionary with the most counties
-    
-    #Return the state with the most counties
-    
-    
-def your_interesting_demographic_function(counties):
-    first = counties[0]["Age"]["Percent Under 18 Years"]
-    second = counties[0]["County"]
-    third = counties[0]["State"]
+    options = ""
+    state = ""
     for c in counties:
-        if c["Age"]["Percent Under 18 Years"] < first:
-            first = c["Age"]["Percent Under 18 Years"] 
-            second = c["County"]
-            third = c["State"]
-    return second, third"""
-     """Compute and return an interesting fact using the demographic data about the counties in the US."""
+        if not c["State"] == state:
+            options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
+            state = c["State"]
+    
+    return options
 
-"""def get_state_options(counties):
-    first = []
-    second = ""
-    for c in counties:
-        first.append(c["state])
-    for c in first:
-        second += Markup("<option value=\"" + c + "\">" + c + "</option>")
-    rendertemplate(demographics.html, list = second)
-    return first                  
-def fun_fact(state):
-    counter = 0;
-    for c in counties:
-        if c["state"] = states[counter]:
-            
-             
-        for c in first:
-            second += second + c
-    rendertemplate(demographics.html, first) """                      
-                     
-  
+def your_interesting_demographic_function(stateName):
+    count = 0
+    while not counties[count]["State"] == stateName:
+        count += 1
+           
+    state = counties[count]["State"] 
+    county = counties[count]["County"]
+    word = "Employment: Private Non-farm Establisment: "
+    employ = counties[count]["Employment"]["Private Non-farm Establishments"]
+    return state + ": " + county + ": " + word + str(employ)
 
+
+def your_interesting_demographic_function2(stateName):
+    countBegin = 0
+    countEnd = 0
+    while not counties[countBegin]["State"] == stateName:
+        countBegin += 1
+    while counties[countBegin + countEnd]["State"] == stateName:
+       countEnd += 1
+       
+    countyNum = randint(countBegin, countBegin+countEnd)
+    countyDem = {c: v for (c, v) in counties[countyNum].items() if not c == stateName or c == counties[countyNum]["County"]}
+     
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
